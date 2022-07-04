855e88063f6407ba94909416f2012dd75d707baa
Fred Lee <juhee.fred.lee@gmail.com>
2022-07-04 14:28:31 +0900

    Initial commit

---
+++ src/main/java/hello/springwarmups/SpringWarmupsApplication.java
@@ -0,0 +1,13 @@
+package hello.springwarmups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWarmupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWarmupsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springwarmups/SpringWarmupsApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springwarmups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWarmupsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
